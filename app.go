302b660bcccf8da3ee29ae6dfde4ee5aa086984a
Andrew Clissold <andrewclissold@gmail.com>
2014-03-28 00:41:40 -0400

    Use markdown and a syntax highlighter on Snips
    With a little bit of cleanup, this architecture should work pretty well
    for the other post-based pages too.

--- app.go
+++ app.go
@@ -1,7 +1,9 @@
 package andrewclissold
 
+import "github.com/russross/blackfriday"
 import "html/template"
 import "net/http"
+import "os"
 import "strings"
 
 func init() {
@@ -10,7 +12,7 @@ func init() {
 	http.HandleFunc("/code", pageHandler)
 	http.HandleFunc("/theory", pageHandler)
 	http.HandleFunc("/music", pageHandler)
-	http.HandleFunc("/snips", pageHandler)
+	http.HandleFunc("/snips", postHandler)
 
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
@@ -33,6 +35,35 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "footer.html", title)
 }
 
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	title := strings.ToUpper(string(r.URL.Path[1])) + r.URL.Path[2:]
+
+	file, err := os.Open(r.URL.Path[1:] + ".md")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fi, err := file.Stat()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	data := make([]byte, fi.Size())
+	file.Read(data)
+
+	htmlFlags := 0
+	htmlFlags |= blackfriday.HTML_GITHUB_BLOCKCODE
+	htmlFlags |= blackfriday.HTML_USE_SMARTYPANTS
+	htmlFlags |= blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
+	renderer := blackfriday.HtmlRenderer(htmlFlags, "", "")
+
+	markdown := blackfriday.Markdown(data, renderer, blackfriday.EXTENSION_FENCED_CODE)
+
+	templates.ExecuteTemplate(w, "header.html", &info{title, ie})
+	w.Write(markdown)
+	templates.ExecuteTemplate(w, "footer.html", title)
+}
+
 type info struct {
 	Title string
 	IE    template.HTML
@@ -47,6 +78,6 @@ var ie template.HTML = `
 var templates = template.Must(template.ParseFiles(
 	"header.html",
 
-	"code.html", "theory.html", "music.html", "snips.html",
+	"code.html", "theory.html", "music.html",
 
 	"footer.html"))
---
+++ snips.md
@@ -0,0 +1,14 @@
+# Random Snippets of Thoughts and Quotes and Stuff
+
+> The easiest way to change the world is to stand on your head.
+>
+> <small>Anonymous</small>
+
+* * *
+
+The beauty of Python is in its conciseness. Like being able to iterate over
+the contents of a file with just a single line.
+
+``` python
+for line in open('filename', 'r'): print(line, end='')
+```
